d644a9f9cdc53211d8c838f3f473240ab9fc626a
link <link@fresh-linkslap>
2016-06-07 20:40:35 +0200

    Changed console coloring (now using termcolor). Now colorsupport for up to 3 robots

--- Maze.h
+++ Maze.h
@@ -4,8 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
-class Roboter;
+#include "Roboter.h"
 
 using namespace std;
 
@@ -25,10 +24,12 @@ private:
     const char free = ' ';
     int startX = 0, startY = 0, endX = 0, endY = 0;
     vector<vector<int>> board;
+
+    void print();
     const bool isEmpty(const Roboter *roboter, const int rx, const int ry);
     void addLine(vector<int> line);
     const int getIntAt(const int x, const int y);
-    const char getCharAt(const int x, const int y);
+    const char printBlock(const int x, const int y);
     void setIntAt(const int x, const int y, const int value);
     void getStartAndEnd();
 };
--- main.cpp
+++ main.cpp
@@ -6,27 +6,6 @@
 
 using namespace std;
 
-const string COLOR_BLACK = "0";
-const string COLOR_BLUE = "1";
-const string COLOR_GREEN = "2";
-const string COLOR_AQUA = "3";
-const string COLOR_RED = "4";
-const string COLOR_PURPLE = "5";
-const string COLOR_YELLOW = "6";
-const string COLOR_WHITE = "7";
-const string COLOR_GRAY = "8";
-const string COLOR_LIGHT_BLUE = "9";
-const string COLOR_LIGHT_GREEN = "A";
-const string COLOR_LIGHT_AQUA = "B";
-const string COLOR_LIGHT_RED = "C";
-const string COLOR_LIGHT_PURPLE = "D";
-const string COLOR_LIGHT_YELLOW = "E";
-const string COLOR_BRIGHT_WHITE = "F";
-
-void setTerminalColor(string textColor, string backgroundColor) {
-    system("COLOR " + backgroundColor + textColor);
-}
-
 int main(int argc, char** argv) {
     Maze m1("maze_tests/maze1_small.txt");
     Maze m2("maze_tests/maze2_unicursal.txt");
