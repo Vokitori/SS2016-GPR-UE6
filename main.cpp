879c8de0bc47c0b945e907239f96a2d03f84d5b4
link <link@fresh-linkslap>
2016-06-08 13:12:30 +0200

    Commented erroneous code.

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-    string filename = argv[1];
-            
+   // string filename = argv[1];
+    
     Maze m1("maze_tests/maze1_small.txt");
     Maze m2("maze_tests/maze2_unicursal.txt");
     Maze m3("maze_tests/maze3_braid.txt");
